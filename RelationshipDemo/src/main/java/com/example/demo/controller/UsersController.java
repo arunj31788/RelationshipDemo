84f060104841619a4d89e763b4e6bfbc45854f4a
vasu <vasu@LAPTOP-7MA9EGPL>
2020-08-06 14:59:49 +0530

    first commit

---
+++ RelationshipDemo/src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Users;
+import com.example.demo.repository.UsersJpaRepository;
+
+@RestController
+@RequestMapping("/empDetails")
+public class UsersController {
+
+	@Autowired
+	private UsersJpaRepository usersJpaRepository;
+
+	@GetMapping(value = "/getAll")
+	public List<Users> getAll() {
+		return usersJpaRepository.findAll();
+	}
+
+	@GetMapping(value = "/getById/{id}")
+	public Optional<Users> getById(@PathVariable("id") Long id) {
+		Optional<Users> user = usersJpaRepository.findById(id);
+		return user;
+	}
+
+	@PostMapping(value = "/save")
+	public Users save(@RequestBody Users users) {
+		return usersJpaRepository.save(users);
+	}
+
+	@PutMapping(value = "/update")
+	public Users update(@RequestBody Users users) {
+		return usersJpaRepository.save(users);
+	}
+
+	@DeleteMapping(value = "/delete")
+	public Users delete(@RequestBody Users users) {
+		usersJpaRepository.delete(users);
+		return users;
+	}
+
+}
---
+++ RelationshipDemo/src/main/java/com/example/demo/model/Users.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class Users{
+	
+	@Id
+	private Long id;
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
